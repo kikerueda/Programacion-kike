e6eb5900c64fa61f621c5dcd6ac0f6542bf58e71
KIKE <kikeruedadaw@gmail.com>
2022-11-15 12:23:17 +0100

    Actualizar

--- Boletin_8/Ejercicio_5.py
+++ Boletin_8/Ejercicio_5.py
@@ -8,12 +8,12 @@ Created on Nov 14, 2022
 # contenido sea igual a la original pero invertida. Así, dada la lista [‘Di’, ‘buen’, ‘día’, ‘a’,
 # ‘papa’], deberá devolver [‘papa’, ‘a’, ‘día’, ‘buen’, ‘Di’].
 #===============================================================================
-numeros=[1,2,3,4,5,6,7,8,9]
-def reversar(lista):
-    for i in range(len(lista)):    
-        guardar=lista[i] 
-        escribir=lista[-i]
-        
-    return lista
-    
-print(reversar(numeros))
+#Ejercicio 5
+###Invertir lista
+caracteres=[1,2,3,4,5,6,7,8,9]
+def revertir(lista):
+    invertida = []
+    for i in range(1,len(lista)+1):
+        invertida.append(lista[-i])
+    return invertida
+print(revertir(caracteres))
